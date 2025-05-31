e79c33ec9deea3169a2fd0a786ab11f1a0a440f5
Saria Khatun <sariajuiit@gmail.com>
2025-05-31 22:33:42 +0600

    added some operation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, Collection, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 let app = express();
@@ -27,12 +27,56 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    let coffeesCollection = client.db('coffeeDB').collection('coffees')
+
+
+    app.get('/coffees',async(req,res)=>{
+        let cursor = coffeesCollection.find();
+        let result = await cursor.toArray();
+        res.send(result);
+    })
+    
+    app.get('/coffees/:id',async(req,res)=>{
+        let id = req.params.id;
+        let query = {_id: new ObjectId(id)}
+        let result = await coffeesCollection.findOne(query)
+        res.send(result)
+    })
+
+    app.post('/coffees',async(req,res)=>{
+        let newCoffee = req.body
+        console.log(newCoffee)
+        let result = await coffeesCollection.insertOne(newCoffee)
+        res.send(result)
+        
+    })
+
+    app.put('/coffees/:id',async(req,res)=>{
+        let id = req.params.id
+        let query = {_id: new ObjectId(id)}
+        let updatedCoffee = req.body
+        let updatedDoc = {
+            $set : updatedCoffee
+        }
+        let result = await coffeesCollection.updateOne(query,updatedDoc)
+        res.send(result);
+
+    })
+
+    app.delete('/coffees/:id',async(req,res)=>{
+            let id = req.params.id
+            let query = {_id : new ObjectId(id)}
+            let result = await coffeesCollection.deleteOne(query)
+            res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
